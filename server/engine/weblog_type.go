48bba7d30ad2fe7f6a7228cfc76a14ec6539fa7d
jdaeman <39020358+jdaeman@users.noreply.github.com>
2022-09-05 17:37:20 +0900

    fix weblog type

--- server/engine/weblog_type.go
+++ server/engine/weblog_type.go
@@ -1,9 +1,12 @@
 package engine
 
 type WebLogData struct {
+	// by client
 	Timestamp   string `json:"timestamp"`
 	Url         string `json:"url"`
 	TabId       int    `json:"tab_id"`
 	RequestId   int    `json:"request_id"`
+	// by server
 	HostAddress string `json:"host_address"`
+	UserAgent   string `json:"user_agent"`
 }
