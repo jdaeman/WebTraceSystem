110130b2e67dd112086526fec90f99b60e0b8e2a
jdaeman <xordn25@naver.com>
2023-01-31 22:59:46 +0900

    docker1

--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"engine"
+	"fmt"
+	"os"
 )
 
 func parseConfig() {
@@ -9,6 +11,12 @@ func parseConfig() {
 }
 
 func main() {
+	if len(os.Args) > 1 {
+		if os.Args[1] == "--help" {
+			fmt.Println("help message")
+			os.Exit(0)
+		}
+	}
 	config := engine.ServerConfig{}
 	config.Port = 8080
 	config.KafkaConf = engine.KafkaConfig{Host: "127.0.0.1:29092", Topic: "my-log"}
