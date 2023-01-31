a362763cb7140aba9ea768b2a726b136fa2ee241
jdaeman <xordn25@naver.com>
2023-01-31 23:21:34 +0900

    fix program argument

--- server/main.go
+++ server/main.go
@@ -11,14 +11,17 @@ func parseConfig() {
 }
 
 func main() {
+	kafkaHost := "localhost:9092"
 	if len(os.Args) > 1 {
 		if os.Args[1] == "--help" {
-			fmt.Println("help message")
+			fmt.Println("Run with argument => kafkahost:port")
 			os.Exit(0)
+		} else {
+			kafkaHost = os.Args[1]
 		}
 	}
 	config := engine.ServerConfig{}
 	config.Port = 8080
-	config.KafkaConf = engine.KafkaConfig{Host: "127.0.0.1:29092", Topic: "my-log"}
+	config.KafkaConf = engine.KafkaConfig{Host: kafkaHost, Topic: "my-log"}
 	engine.RunServer(config)
 }
